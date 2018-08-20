aac6afbeba760883a125c1af16950dce70d5ca6b
peram.swarupa <41728997+swarupareddyperam@users.noreply.github.com>
2018-08-20 11:42:59 +0530

    Create 106.java

---
+++ 106.java
@@ -0,0 +1,28 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  int n,k;
+		Scanner sc=new Scanner(System.in);
+		n=sc.nextInt();
+		k=sc.nextInt();
+		int a[];
+		a=new int[n];
+		for(int i=0;i<n;i++)
+		{
+			a[i]=sc.nextInt();
+		}
+		if(a[k]%2!=0)
+		{
+		System.out.print(a[k]);	
+		}
+		
+	}
+}
